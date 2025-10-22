d4e0dce71479bbb4937468ab594139f215237fdb
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-22 14:38:13 -0300

    nit: usa arrays de validação ao invés de strings em StorePetRequest

--- app/Http/Requests/Pet/StorePetRequest.php
+++ app/Http/Requests/Pet/StorePetRequest.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StorePetRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name'      => 'required|string|max:255',
+            'species'   => 'nullable|string|max:100',
+            'breed'     => 'nullable|string|max:100',
+            'birthdate' => 'nullable|date',
         ];
     }
 }
