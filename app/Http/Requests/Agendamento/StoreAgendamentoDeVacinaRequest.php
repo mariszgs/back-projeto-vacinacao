fae993f1645d01ae502b119e75a78ded65c58ed6
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-22 14:55:20 -0300

    nit: usa arrays de validação ao invés de strings em StoreAgendamentoDeVacinaRequest

--- app/Http/Requests/Agendamento/StoreAgendamentoDeVacinaRequest.php
+++ app/Http/Requests/Agendamento/StoreAgendamentoDeVacinaRequest.php
@@ -6,26 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreAgendamentoDeVacinaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-     public function rules()
+  
+     public function rules(): array
     {
         return [
-            'pet_id' => 'required|exists:pets,id',
-            'vacina_id' => 'required|exists:vacinas,id',
-            'data_agendada' => 'required|date|after:now',
-            'observacoes' => 'nullable|string',
+            'pet_id' => ['required','exists:pets,id'],
+            'vacina_id' => ['required','exists:vacinas,id'],
+            'data_agendada' => ['required','date','after:now'],
+            'observacoes' => ['nullable','string'],
         ];
     }
 }
