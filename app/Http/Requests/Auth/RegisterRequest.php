305a9e2912f4935ac0f5724ac47e049daa46e661
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-23 14:11:42 -0300

    feat: adicionar validação RegisterAuthRequest para registro de usuário

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterAuthRequest extends FormRequest
+{
+   
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+}
