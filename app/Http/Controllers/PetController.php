b970f7fcde146a70d8eba3e1db8c35b880e147b8
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-23 13:30:20 -0300

    refactor: refatorando petcontroller

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -5,38 +5,34 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Pet\StorePetRequest;
 use App\Http\Requests\Pet\UpdatePetRequest;
 use App\Http\Resources\PetResource;
-use App\Models\Pet;
 use App\Services\Pet\IndexPetService;
 use App\Services\Pet\ShowPetService;
 use App\Services\Pet\StorePetService;
 use App\Services\Pet\UpdatePetService;
 use App\Services\Pet\DeletePetService;
 use Illuminate\Http\Request;
+use App\Models\Pet;
 
 class PetController extends Controller
 {
-  public function index(IndexPetService $service, Request $request)
-{
-    $pets = $service->run($request);
-    return PetResource::collection($pets);
-}
-
+    public function index(IndexPetService $service, Request $request)
+    {
+        $pets = $service->run($request);
+        return PetResource::collection($pets);
+    }
 
     public function show(Pet $pet, ShowPetService $service)
     {
         $pet = $service->run($pet);
-        return new PetResource ($pet);
+        return new PetResource($pet);
     }
 
     public function store(StorePetRequest $request, StorePetService $service)
-{
-    dd('chegou no controller');
-    
-    $data = $request->validated();
-    $pet = $service->run($data);
-    return response(new PetResource($pet), 201);
-}
-
+    {
+        $data = $request->validated();
+        $pet = $service->run($data);
+        return response(new PetResource($pet), 201);
+    }
 
     public function update(UpdatePetRequest $request, Pet $pet, UpdatePetService $service)
     {
@@ -48,6 +44,6 @@ class PetController extends Controller
     public function destroy(Pet $pet, DeletePetService $service)
     {
         $service->run($pet);
-        return response()->json(['message' => 'Pet deletado!'], 200);
+        return response()->json(['message' => 'Pet deletado com sucesso!'], 200);
     }
 }
