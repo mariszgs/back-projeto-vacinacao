bc12ab15b0236f391cbd646700edb2596418e5f6
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-23 14:13:54 -0300

    feat: adicionar RegisterAuthService para registrar usuário e criar token

---
+++ app/Services/Auth/RegisterAuthService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterAuthService
+{
+    public function run(array $data)
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+}
