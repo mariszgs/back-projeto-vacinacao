11fd6dee7bc29f2882984ccb349dd59085bfe46b
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-28 13:25:06 -0300

    feat: adiciona UpdateUserRequest com regras de validação

---
+++ app/Http/Requests/User/UserUpdateRequest.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        $user = $this->user();
+        
+        return [
+            'name' => ['sometimes', 'string', 'max:255'],
+            'email' => ['sometimes', 'email', 'unique:users,email,' . $user->id],
+            'password' => ['sometimes', 'min:6', 'confirmed'],
+        ];
+    }
+}
