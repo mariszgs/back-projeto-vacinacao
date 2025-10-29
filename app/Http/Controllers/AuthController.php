c4e87139fb77fc515fde6cb761889da38346b282
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-10-29 14:17:38 -0300

    fix: corrigir namespace e importações no controller e request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,32 +1,32 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\Auth\RegisterRequest;
-use App\Services\Auth\LoginService;
-use App\Services\Auth\LogoutService;
-use App\Services\Auth\RegisterService;
-use Illuminate\Http\Request;
+    use App\Http\Requests\Auth\LoginRequest;
+    use App\Http\Requests\Auth\RegisterRequest;
+    use App\Services\Auth\LoginService;
+    use App\Services\Auth\LogoutService;
+    use App\Services\Auth\RegisterService;
+    use Illuminate\Http\Request;
 
-class AuthController extends Controller
-{
-   public function register(RegisterRequest $request, RegisterService $service)
-{
-    $data = $request->validated();
-    $user = $service->run($data);
-    return response($user);
-}
-    public function login(LoginRequest $request, LoginService $service)
+    class AuthController extends Controller
+    {
+    public function register(RegisterRequest $request, RegisterService $service)
     {
         $data = $request->validated();
-        $response = $service->run($data);
-        return response()->json($response);
+        $user = $service->run($data);
+        return response($user);
     }
+        public function login(LoginRequest $request, LoginService $service)
+        {
+            $data = $request->validated();
+            $response = $service->run($data);
+            return response()->json($response);
+        }
 
-    public function logout(Request $request, LogoutService $service)
-    {
-        $response = $service->run($request);
-        return response()->json($response);
+        public function logout(Request $request, LogoutService $service)
+        {
+            $response = $service->run($request);
+            return response()->json($response);
+        }
     }
-}
